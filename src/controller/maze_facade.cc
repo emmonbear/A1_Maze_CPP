4291c541b429ea6af2f45c6bcc6daae217067a5c
emmonbea <moskaleviluak@icloud.com>
2024-11-04 20:07:24 +0500

    feat(controller): add MazeFacade

---
+++ src/controller/maze_facade.cc
@@ -0,0 +1,28 @@
+/**
+ * @file maze_facade.cc
+ * @author Moskalev Ilya (moskalevilua1998@gmail.com)
+ * @brief Implementation file for the MazeFacade class
+ * @version 1.0
+ * @date 2024-11-04
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#include "include/controller/maze_facade.h"
+
+#include <string>
+
+namespace s21 {
+
+void MazeFacade::generate(int rows, int cols) {
+  maze_.set_rows(rows);
+  maze_.set_cols(cols);
+  maze_.generate();
+}
+
+void MazeFacade::loadFromFile(const std::string& filename) {
+  maze_.loadFromFile(filename);
+}
+
+}  // namespace s21
---
+++ src/include/controller/maze_facade.h
@@ -0,0 +1,33 @@
+/**
+ * @file maze_facade.h
+ * @author Moskalev Ilya (moskalevilua1998@gmail.com)
+ * @brief Header file for the MazeFacade class
+ * @version 1.0
+ * @date 2024-11-04
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#ifndef SRC_INCLUDE_CONTROLLER_MAZE_FACADE_H_
+#define SRC_INCLUDE_CONTROLLER_MAZE_FACADE_H_
+
+#include <string>
+
+#include "include/model/maze.h"
+
+namespace s21 {
+
+class MazeFacade {
+ public:
+  void generate(int rows, int cols);
+  void loadFromFile(const std::string& filename);
+
+  inline const Maze& maze() const { return maze_; }
+
+ private:
+  Maze maze_{};
+};
+}  // namespace s21
+
+#endif  // SRC_INCLUDE_CONTROLLER_MAZE_FACADE_H_
