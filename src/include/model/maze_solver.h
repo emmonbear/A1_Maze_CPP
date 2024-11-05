a6a9aecac7b817ca7d08697bcfc06c9a9b26dcbf
emmonbea <moskaleviluak@icloud.com>
2024-11-05 10:54:01 +0500

    feat(model): add MazeSolver class

---
+++ src/include/model/maze_solver.h
@@ -0,0 +1,21 @@
+/**
+ * @file maze_solver.h
+ * @author Moskalev Ilya (moskalevilua1998@gmail.com)
+ * @brief Header file for the MazeSolver class
+ * @version 1.0
+ * @date 2024-11-05
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#ifndef SRC_INCLUDE_MODEL_MAZE_SOLVER_H_
+#define SRC_INCLUDE_MODEL_MAZE_SOLVER_H_
+
+#include "include/model/maze.h"
+
+namespace s21 {
+class MazeSolver {};
+}  // namespace s21
+
+#endif  // SRC_INCLUDE_MODEL_MAZE_SOLVER_H_
---
+++ src/model/maze_solver.cc
@@ -0,0 +1,14 @@
+/**
+ * @file maze_solver.cc
+ * @author Moskalev Ilya (moskalevilua1998@gmail.com)
+ * @brief Implementation file for the MazeSolver class
+ * @version 1.0
+ * @date 2024-11-05
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#include "include/model/maze_solver.h"
+
+namespace s21 {}
